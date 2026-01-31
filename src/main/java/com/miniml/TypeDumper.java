11e082e8695e191d2d63dacbfa258b1af69e1c8f
mark frawley <mark.frawley@moia.io>
2026-01-31 01:09:24 +0100

    wip
    Signed-off-by: mark frawley <mark.frawley@moia.io>

---
+++ src/main/java/com/miniml/TypeDumper.java
@@ -0,0 +1,69 @@
+package com.miniml;
+
+import java.util.Map;
+import java.util.Set;
+
+public class TypeDumper {
+    
+    public static void dumpModule(Module module, Map<Expr, Type> typeMap, Map<String, Set<Type>> instantiations) {
+        System.out.println("=== MODULE DUMP ===");
+        System.out.println("\nImports: " + module.imports());
+        
+        System.out.println("\nDeclarations:");
+        for (Module.TopLevel decl : module.declarations()) {
+            if (decl instanceof Module.TopLevel.FnDecl(String name, var params, Expr body)) {
+                System.out.println("  fn " + name + " " + params + " =");
+                dumpExpr(body, typeMap, 4);
+                Type bodyType = typeMap.get(body);
+                System.out.println("    : " + bodyType);
+                
+                if (instantiations.containsKey(name)) {
+                    System.out.println("    instantiations: " + instantiations.get(name));
+                }
+            }
+        }
+        
+        if (module.mainExpr() != null) {
+            System.out.println("\nMain expression:");
+            dumpExpr(module.mainExpr(), typeMap, 2);
+            Type mainType = typeMap.get(module.mainExpr());
+            System.out.println("  : " + mainType);
+        }
+        
+        System.out.println("\n=== END MODULE DUMP ===\n");
+    }
+    
+    private static void dumpExpr(Expr expr, Map<Expr, Type> typeMap, int indent) {
+        String prefix = " ".repeat(indent);
+        Type type = typeMap.get(expr);
+        
+        switch (expr) {
+            case Expr.IntLit(int value) -> 
+                System.out.println(prefix + value + " : " + type);
+            case Expr.FloatLit(double value) ->
+                System.out.println(prefix + value + " : " + type);
+            case Expr.Var(String name) ->
+                System.out.println(prefix + name + " : " + type);
+            case Expr.BinOp(Expr.Op op, Expr left, Expr right) -> {
+                System.out.println(prefix + "(" + op + ")");
+                dumpExpr(left, typeMap, indent + 2);
+                dumpExpr(right, typeMap, indent + 2);
+                System.out.println(prefix + "  => " + type);
+            }
+            case Expr.App(Expr func, var args) -> {
+                System.out.println(prefix + "App:");
+                dumpExpr(func, typeMap, indent + 2);
+                for (Expr arg : args) {
+                    dumpExpr(arg, typeMap, indent + 2);
+                }
+                System.out.println(prefix + "  => " + type);
+            }
+            case Expr.Print(Expr value) -> {
+                System.out.println(prefix + "print:");
+                dumpExpr(value, typeMap, indent + 2);
+                System.out.println(prefix + "  => " + type);
+            }
+            default -> System.out.println(prefix + expr.getClass().getSimpleName() + " : " + type);
+        }
+    }
+}
--- stdlib/Math.ml
+++ stdlib/Math.ml
@@ -1,19 +1,19 @@
-fn abs (x: double) = if x < 0.0 then 0.0 - x else x;
+fn abs (x: double) : double = if x < 0.0 then 0.0 - x else x;
 
-fn max (a: double) (b: double) = if a > b then a else b;
+fn max (a: double) (b: double) : double = if a > b then a else b;
 
-fn min (a: double) (b: double) = if a < b then a else b;
+fn min (a: double) (b: double): double = if a < b then a else b;
 
-fn square (x: double) = x * x;
+fn square (x: double): double = x * x;
 
-fn sqrt (x: double) = java_call "java.lang.Math" "sqrt" x;
+fn sqrt (x: double) : double= java_call "java.lang.Math" "sqrt" x;
 
-fn sin (x: double) = java_call "java.lang.Math" "sin" x;
+fn sin (x: double) : double = java_call "java.lang.Math" "sin" x;
 
-fn cos (x: double) = java_call "java.lang.Math" "cos" x;
+fn cos (x: double) : double = java_call "java.lang.Math" "cos" x;
 
-fn tan (x: double) = java_call "java.lang.Math" "tan" x;
+fn tan (x: double) : double = java_call "java.lang.Math" "tan" x;
 
-fn log (x: double) = java_call "java.lang.Math" "log" x;
+fn log (x: double) : double = java_call "java.lang.Math" "log" x;
 
-fn exp (x: double) = java_call "java.lang.Math" "exp" x;
+fn exp (x: double) : double = java_call "java.lang.Math" "exp" x;
